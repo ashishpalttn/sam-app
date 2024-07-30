13f342e5b62877b96c100d6a1de9869ad38c66c1
Ashish <ashish.p@lecturenotes.in>
2024-07-30 23:26:53 +0530

    first commit 30July/2024

---
+++ .aws-sam/build/HelloWorldFunction/app.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk');
+let response;
+
+exports.lambdaHandler = async (event, context) => {
+
+    AWS.config.update({region: 'ap-south-1'});
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    const dynamodbTableName = 'sam-deploy-third-DynamoDBTable-P9TPLDV1OVEF';    
+    const params = {
+      TableName: dynamodbTableName,
+      Item: {
+        id: "1",
+        info: {
+          name: "Ashish Pal",
+          email: "ashishpal778@gmail.com",
+          age: 29,
+          address: "India"
+        },
+      },
+    };
+    try{
+        await dynamo.put(params).promise();
+        console.log("Data inserted Successfully");
+    }
+    catch(err){
+        console.log('Error while inserting the data: ',err);
+    }
+
+    // const data = await dynamo.scan({ TableName: dynamodbTableName }).promise();
+
+
+    try {
+        // const ret = await axios(url);
+        response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "hello world asm-app today is 30 July 2024",
+            // location: ret.data.trim()
+          }),
+        };
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
+
